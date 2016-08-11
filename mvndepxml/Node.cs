0cb2b7ad99efa7eddde32f520a9eedde8c0292f8
Chris KANG <chriskang@ea.com>
2016-08-11 18:19:32 +0800

    first commit

---
+++ mvndepxml/Node.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace mvndepxml
+{
+    public class Node
+    {
+        public string Content { get; private set; }
+
+        public int Depth { get; private set; }
+
+        public Node Parent { get; private set; }
+
+        public IList<Node> Children { get; private set; }
+
+        private Node(string content, Node parent, int depth)
+        {
+            if (string.IsNullOrEmpty(content)) throw new ArgumentNullException("content");
+            Content = content;
+            Parent = parent;
+            Depth = depth;
+            Children = new List<Node>();
+        }
+
+        public static Node Load(string file)
+        {
+            return GenerateNode(File.ReadAllLines(file));
+        }
+
+        private static Node GenerateNode(IEnumerable<string> text)
+        {
+            var parent = new Node("dependencies", null, -1);
+            text.Aggregate(parent, (current, s) => Generate(s, current));
+            
+            return parent.Children != null && parent.Children.Count() == 1 ? parent.Children[0] : null;
+        }
+
+        private const string ChildIndicator = "+- ";
+        private const string LastChildIndicator = "\\- ";
+        private static readonly string[] Indents = { ChildIndicator, LastChildIndicator, "|  ", "   " };
+        private const int IndicatorLen = 3;
+
+        private static Node Generate(string content, Node previous)
+        {
+            var currentLine = content;
+            var depth = GetDepth(ref currentLine);
+            var newParent = previous;
+            if (previous == null) throw new InvalidDataException("previous node is null");
+            if (depth - previous.Depth > 2)
+            {
+                throw new InvalidDataException(string.Format("Depth of {0} is {1} where previous {2} depth is {3}",
+                    content, depth, previous.Content, previous.Depth));
+            }
+            if (depth == previous.Depth)
+            {
+                newParent = previous.Parent;
+            }
+            else if (depth == previous.Depth + 1)
+            {
+                newParent = previous;
+            }
+            else
+            {
+                for (var i = depth - previous.Depth + 1; i > 0; --i)
+                {
+                    newParent = newParent.Parent;
+                }
+            }
+            var newNode = new Node(currentLine, newParent, depth);
+            newParent.Children.Add(newNode);
+            return newNode;
+        }
+
+        private static int GetDepth(ref string content)
+        {
+            var depth = 0;
+            for (; Indents.Any(content.StartsWith); content = content.Substring(IndicatorLen), ++depth) ;
+            return depth;
+        }
+
+        public override string ToString()
+        {
+            return Content;
+        }
+
+        public void Print(Action<Node> action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+            RecursivelyPrint(action, this);
+        }
+
+        private static void RecursivelyPrint(Action<Node> action, Node current)
+        {
+            if (current == null) throw new ArgumentNullException("current");
+            action(current);
+            if (current.Children == null || !current.Children.Any()) return;
+            foreach (var child in current.Children)
+            {
+                RecursivelyPrint(action, child);
+            }
+        }
+    }
+}
---
+++ mvndepxml/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace mvndepxml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new ArgumentException("please pass in result file of running 'mvn dependency:tree -Dverbose -DoutputFile=%res_file%");
+           GenerateXml(args[0]);
+        }
+
+        static void GenerateXml(string fileName)
+        {
+            var root = Node.Load(fileName);
+            root.Print(n =>
+            {
+                for(var i = 0; i < n.Depth; ++i) Console.Write('-');
+                Console.WriteLine(n.Content);
+            });
+        }
+
+    }
+}
